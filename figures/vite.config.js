17b954febe82e73c304e7e11d6b7b798efb5613f
sashtje <sashtje.lefevre@gmail.com>
2024-09-26 13:24:09 +0300

    fix: fix path

--- figures/vite.config.js
+++ figures/vite.config.js
@@ -1,7 +1,7 @@
 export default {
   root: "src/",
   publicDir: "../static/",
-  base: process.env.NODE_ENV === "production" ? "/threejs/figures/" : "./",
+  base: "./",
   server: {
     host: true,
     open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env),
